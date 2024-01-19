395fc9c050dd66948bfb3f4f09535c96c961e761
Johnny Lim <izeye@naver.com>
2024-01-19 21:29:42 +0900

    Add tests for SimpleMeterRegistry

---
+++ src/test/java/leaningtest/io/micrometer/core/instrument/simple/SimpleMeterRegistryTests.java
@@ -0,0 +1,28 @@
+package leaningtest.io.micrometer.core.instrument.simple;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link SimpleMeterRegistry}.
+ *
+ * @author Johnny Lim
+ */
+class SimpleMeterRegistryTests {
+
+    private final SimpleMeterRegistry meterRegistry = new SimpleMeterRegistry();
+
+    @Test
+    void test() {
+        String name = "my.counter";
+        Counter counter = Counter.builder(name).register(this.meterRegistry);
+        counter.increment();
+
+        Counter found = this.meterRegistry.counter(name);
+        assertThat(found.count()).isOne();
+    }
+
+}
